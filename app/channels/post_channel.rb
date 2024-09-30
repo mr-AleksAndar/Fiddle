09c377ce84d0da106fd0da1b39b01c031bab36a9
mr-AleksAndar <aleks@weareevermore.com>
2024-09-30 16:47:06 +0300

    fix postchannel

--- app/channels/post_channel.rb
+++ app/channels/post_channel.rb
@@ -1,4 +1,13 @@
-class PostsChannel < ApplicationCable::Channel
-    def subscribed
-    end
+# app/channels/post_channel.rb
+
+class PostChannel < ApplicationCable::Channel
+  def subscribed
+    # Any setup needed when the channel is subscribed
   end
+
+  def unsubscribed
+    # Any cleanup needed when the channel is unsubscribed
+  end
+
+  # Additional methods as needed
+end
